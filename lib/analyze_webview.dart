a218a51f9bbfdb406d16afaf9b245624ee73d201
leo.lu <leo.lu@blackshark.com>
2021-11-16 16:41:31 +0800

    null safaty

--- lib/analyze_webview.dart
+++ lib/analyze_webview.dart
@@ -5,8 +5,8 @@ import 'package:flutter/services.dart';
 class AnalyzeWebview {
   static const MethodChannel _channel = const MethodChannel('tech.gentleflow.analyze_webview');
 
-  static Future<String> loadUrl(String url, {String userAgent, String sourceRegex}) async {
-    if (url == null || url == "") {
+  static Future<String> loadUrl(String url, {String? userAgent, String? sourceRegex}) async {
+    if (url == "") {
       return "请求失败";
     }
     print("AnalyzeWebView  Flutter loadUrl $url");
